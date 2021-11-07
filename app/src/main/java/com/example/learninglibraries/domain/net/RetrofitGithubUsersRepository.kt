8e2c6942fd87572c00651c964bc7a64fec5cdcbe
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-11-07 13:55:49 +0300

    Get github users by detecting the presence of a network on the device

--- app/src/main/java/com/example/learninglibraries/domain/net/RetrofitGithubUsersRepository.kt
+++ app/src/main/java/com/example/learninglibraries/domain/net/RetrofitGithubUsersRepository.kt
@@ -1,14 +1,56 @@
 package com.example.learninglibraries.domain.net
 
+import com.example.learninglibraries.domain.database.Database
+import com.example.learninglibraries.domain.database.entities.RoomGithubUser
 import com.example.learninglibraries.domain.net.data.GithubUser
 import com.example.learninglibraries.domain.net.data.GithubUserRepos
+import com.example.learninglibraries.ui.INetworkStatus
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class RetrofitGithubUsersRepository(private val api: IDataSource) : IGithubUserRepository {
+class RetrofitGithubUsersRepository(
+    private val api: IDataSource,
+    private val networkStatus: INetworkStatus,
+    private val database: Database
+) : IGithubUserRepository {
+
     override fun getGithubUsers(): Single<List<GithubUser>> =
-        api.getGithubUsers().subscribeOn(Schedulers.io())
+        networkStatus.isOnlineSingle().flatMap { isOnline ->
+            if (isOnline) {
+                getGithubUserFromExternalServer()
+            } else {
+                getGithubUserFromLocal()
+            }
+        }.subscribeOn(Schedulers.io())
 
     override fun getGithubUserRepos(url: String): Single<List<GithubUserRepos>> =
         api.getGithubUserRepos(url).subscribeOn(Schedulers.io())
+
+    private fun getGithubUserFromExternalServer(): Single<List<GithubUser>> =
+        api.getGithubUsers().flatMap { users ->
+            Single.fromCallable {
+                val roomUsers = users.map { user ->
+                    RoomGithubUser(
+                        user.id.toString(),
+                        user.login ?: "",
+                        user.avatarUrl ?: "",
+                        user.reposUrl ?: ""
+                    )
+                }
+                database.githubUserDao.insert(roomUsers)
+                users
+            }
+        }
+
+    private fun getGithubUserFromLocal(): Single<List<GithubUser>> =
+        Single.fromCallable {
+            database.githubUserDao.getAll().map { roomGithubUser ->
+                GithubUser(
+                    id = roomGithubUser.id.toInt(),
+                    login = roomGithubUser.login,
+                    avatarUrl = roomGithubUser.avatarUrl,
+                    reposUrl = roomGithubUser.reposUrl
+                )
+            }
+        }
 }
---
+++ app/src/main/java/com/example/learninglibraries/ui/AndroidNetworkStatus.kt
@@ -0,0 +1,38 @@
+package com.example.learninglibraries.ui
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+
+class AndroidNetworkStatus(context: Context) : INetworkStatus {
+    private val statusObject: BehaviorSubject<Boolean> = BehaviorSubject.create()
+
+    init {
+        statusObject.onNext(false)
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+            .registerNetworkCallback(
+                NetworkRequest.Builder().build(),
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        statusObject.onNext(true)
+                    }
+
+                    override fun onLost(network: Network) {
+                        statusObject.onNext(false)
+                    }
+
+                    override fun onUnavailable() {
+                        statusObject.onNext(false)
+                    }
+                }
+            )
+    }
+
+    override fun isOnline(): Observable<Boolean> = statusObject
+
+    override fun isOnlineSingle(): Single<Boolean> = statusObject.first(false)
+}
